ad28d47ccea4526d2803241c9358a314cc2bca4c
quyinsong <97971311+quyinsong@users.noreply.github.com>
2023-04-11 19:54:52 +0600

    Add files via upload

---
+++ 论文复现2-GVF-LinearFunction/GVF2.m
@@ -0,0 +1,78 @@
+function rc = GVF2( x,y,psi,beta,rc )
+%GVF 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+% constraints
+X = psi+beta;
+m = [cos(X), sin(X)]';
+E = [0 -1;1 0]; I2 = diag([1 1]);
+kro = 0.05;
+Ud = 0.3;
+% boundary 1
+R1 = 21; % 最外层边界，进入该边界后碰撞行为和跟踪行为同时存在
+d1 = -2; % 内层边界，进入该边界后只存在避碰行为
+phir1 = y+R1-x;
+no1 = [-1 1]'; taur1 = E*no1;
+H1 = [0 0; 0 0];
+kod = -1;
+
+mod1 = kod*taur1-kro*phir1*no1;
+mod1_hat = mod1/norm(mod1);
+
+mc1 = Ud*m;
+mc1_hat = mc1/norm(mc1);
+       
+mod1_dot = (E-kro*phir1*I2)*H1*mc1-kro*no1'*mc1*no1;
+mod1_hat_dot = -E*(mod1_hat*mod1_hat')*E*mod1_dot/norm(mod1);
+psio1_dot = -mod1_hat'*E*mod1_hat_dot;
+r_od1 = (psio1_dot-0.5*mc1_hat'*E*mod1_hat)*norm(mc1)/(Ud*mc1_hat'*m);
+       
+if phir1 <= d1
+    f1 = 0; f2 = 1;
+elseif phir1 < 0
+    l1 = 100; l2 = 5;  % l1 越大则避障优先级越高 l2 越大则路径跟踪优先级越高
+    f1 = exp(l1/(d1-phir1)); f2 = exp(l2/phir1);
+else
+    f1 = 1; f2 = 0;
+end
+si1 = f2/(f1+f2); zi1 = f1/(f1+f2);
+
+% boundary 2
+R2 = 18; % 最外层边界，进入该边界后碰撞行为和跟踪行为同时存在
+d2 = -3; % 内层边界，进入该边界后只存在避碰行为
+phir2 = x-y+R2;
+no2 = [1 -1]'; taur2 = E*no1;
+H2 = [0 0; 0 0];
+
+mod2 = kod*taur2-kro*phir2*no2;
+mod2_hat = mod2/norm(mod2);
+
+mc2 = Ud*m;
+mc2_hat = mc2/norm(mc2);
+       
+mod2_dot = (E-kro*phir2*I2)*H2*mc2-kro*no2'*mc2*no2;
+mod2_hat_dot = -E*(mod2_hat*mod2_hat')*E*mod2_dot/norm(mod2);
+psio2_dot = -mod2_hat'*E*mod2_hat_dot;
+r_od2 = (psio2_dot-0.5*mc2_hat'*E*mod2_hat)*norm(mc2)/(Ud*mc2_hat'*m);
+       
+if phir2 <= d2
+    f1 = 0; f2 = 1;
+elseif phir2 < 0
+    l1 = 100; l2 = 5;  % l1 越大则避障优先级越高 l2 越大则路径跟踪优先级越高
+    f1 = exp(l1/(d2-phir1)); f2 = exp(l2/phir1);
+else
+    f1 = 1; f2 = 0;
+end
+si2 = f2/(f1+f2); zi2 = f1/(f1+f2);
+
+rc = si1*r_od1+si2*r_od2+zi1*zi2*rc;
+
+
+
+
+
+
+
+
+end
+
---
+++ 论文复现2-GVF-LinearFunction/PathInfo.m
@@ -0,0 +1,162 @@
+function [wo,thetao,vs, posd, pd_dw] = PathInfo( pc, z_theta, ud, ts )
+%LOS [ud, rd, posd] = LOS( pc, eta, nu, ts )
+% curved path LOS guidace law
+% Author : Yinsong Qu
+% Date : 7 15 2022
+%[xd, yd, psid, psif, xe, ye, w] = LOS2( pc, k1, delta, x, y, psi, U, beta, w, ts )
+% Author : Quyinsong
+% Date: 2022 6 1
+% reference : Path-Following Algorithms and Experiments for an Unmanned Surface Vehicle
+%%%% INPUT:
+% pc : chose curved path
+% -------------------pc-----------------------------------
+% 1: stright line path; 2: sinusoidal path; 3: circle path
+% -------------------pc-----------------------------------
+% eta = [x y psi]' : the position and heading angle of USV
+% nu = [u v r]' : the speed and yaw angular rate
+% ts : sample time
+
+% check input and state dimentions
+if nargin ~=4,error('input number must be 4!');end
+
+% states initializing
+% states initializing
+persistent theta w miu lambda
+if isempty(theta)
+    theta = 0;
+    w = 0;
+    miu = 0;
+    lambda = 0.1;
+end
+
+% curved path
+switch pc
+    case 1
+        %---------------直线路径-----------------
+        xd = 2*w;
+        yd = 2*w;
+        xd_dw = 2;
+        yd_dw = 2;
+        xd_ddw = 0; yd_ddw = 0;
+    case 2
+        %---------------Wang 正弦曲线路径--------------
+        xd = 8*sin(0.5*w) + 4*w;
+        yd = 4*w;
+        xd_dw = 4*cos(0.5*w)+4;
+        yd_dw = 4;
+        xd_ddw = -2*sin(0.5*w); yd_ddw = 0;
+    case 3
+        %---------------圆形路径------------------
+        R = 15;
+        xd = R*cos(w)+0; 
+        yd = R*sin(w)+100; 
+        xd_dw = -R*sin(w); 
+        yd_dw = R*cos(w);
+        xd_ddw = -R*cos(w); yd_ddw = -R*sin(w);
+    case 4
+        %---------------混合路径------------------
+        xd = 10;
+        yd = w;
+        xd_dw = 0; xd_ddw = 0;
+        yd_dw = 1; yd_ddw = 0;
+        if w >= 80 
+            R = 10;
+            xd = R*cos(w-80);
+            yd = R*sin(w-80)+80;
+            xd_dw = -R*sin(w-80); xd_ddw = -R*cos(w-80);
+            yd_dw = R*cos(w-80); yd_ddw = -R*sin(w-80);
+        end
+        if w >= 80+pi
+            xd = -10;
+            yd = -(w-80-pi)+80;
+            xd_dw = 0; xd_ddw = 0;
+            yd_dw = -1; yd_ddw = 0;
+        end
+        if w >= 160+pi
+            xd = R*cos(w-160-pi+pi);
+            yd = R*sin(w-160-pi+pi);
+            xd_dw = -R*sin(w-160-pi+pi); xd_ddw = -R*cos(w-160-pi+pi);
+            yd_dw = R*cos(w-160-pi+pi); yd_ddw = -R*sin(w-160-pi+pi);
+        end
+        if w >= 160+2*pi
+            xd = 10;
+            yd = w-160-2*pi;
+            xd_dw = 0; xd_ddw = 0;
+            yd_dw = 1; yd_ddw = 0;
+        end 
+    case 5
+        xd = 4*cos(w);
+        yd = 3*w;
+        xd_dw = -4*sin(w);
+        yd_dw = 3;
+        xd_ddw = -4*cos(w); yd_ddw = 0;
+    case 6
+        xd = w;
+        yd = w;
+        xd_dw = 1;
+        yd_dw = 1;
+        xd_ddw = 0; yd_ddw = 0;
+        if w>=15
+            xd = 30-w;
+            yd = w;
+            xd_dw = -1;
+            yd_dw = 1;
+            xd_ddw = 0; yd_ddw = 0;
+        end
+    case 7
+        xd = w;
+        yd = w;
+        xd_dw = 1;
+        yd_dw = 1;
+        xd_ddw = 0; yd_ddw = 0;
+end
+
+%---------------------------------------------------------
+kc = abs(xd_dw*yd_ddw-yd_dw*xd_ddw)/sqrt(xd_dw^2+yd_dw^2)^3; % 曲线路径的曲率
+
+psip = atan2(yd_dw,xd_dw);  % 路径虚拟参考点切线与x轴夹角
+persistent psipf
+if isempty(psipf)
+    psipf = psip;
+end
+psipf_dot = -(psipf-psip)/0.1;
+psipf = ts*psipf_dot+psipf;
+kcs = kc*sign(psipf_dot);
+
+% 路径参数的更新
+% 和参考文献中变量略有出入，w->theta, theta->w
+theta_dot = -lambda*(theta+miu*z_theta);
+theta = theta_dot*ts+theta;
+
+vs = ud/sqrt(xd_dw^2+yd_dw^2);
+w_dot = vs-theta;
+w = w_dot*ts+w;
+
+wo = w;
+thetao = theta;
+
+posd = [xd,yd]';
+pd_dw = [xd_dw,yd_dw]';
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
